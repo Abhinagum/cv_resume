a8517efedebe79024b97830a8b708664dc9bdbdb
Abhinag Ujire Mohan <abhinag6@gmail.com>
2019-08-03 19:33:54 +0530

    Updates

---
+++ precache-manifest.2d05ecf4b83401d6f554490219706126.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a749cf99f1d95e9f062c3d5cf78ea2e7",
+    "url": "/cv_resume/index.html"
+  },
+  {
+    "revision": "0b450fd7c07bacd25424",
+    "url": "/cv_resume/static/css/main.584f321a.chunk.css"
+  },
+  {
+    "revision": "197d9ad31ce7bba7869a",
+    "url": "/cv_resume/static/js/2.0ed2724a.chunk.js"
+  },
+  {
+    "revision": "0b450fd7c07bacd25424",
+    "url": "/cv_resume/static/js/main.7cc0aeeb.chunk.js"
+  },
+  {
+    "revision": "874342c9bd422723b7bf",
+    "url": "/cv_resume/static/js/runtime~main.7c9d7915.js"
+  }
+]);
